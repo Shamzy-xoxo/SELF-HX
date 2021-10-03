0520c83522a53a4f232b9636d9aa71530816264b
Shamzy <83776227+Shamzy-xoxo@users.noreply.github.com>
2021-10-03 17:41:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -55,9 +55,9 @@ const { jadibot, stopjadibot, listjadibot } = require('./lib/jadibot')
 
 banChats = true
 offline = false
-targetpc = '6285751056816'
-owner = '6285751056816'
-fake = 'HEXAGONZ'
+targetpc = '917594090074'
+owner = '917594090074'
+fake = 'SHAMZY'
 numbernye = '0'
 waktu = '-'
 alasan = '-'
